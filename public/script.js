7fe4dc2b6b8c4eaef177ad429cca77d9cdde176e
TallaBambo <tallabambo041@gmail.com>
2023-03-01 16:23:32 +0100

    changed home page bg

--- public/script.js
+++ public/script.js
@@ -17,29 +17,3 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     body.classList.toggle('over-flow-hidden')
 })
-
-
-let slideIndex = 0;
-const mySlides = document.querySelector('.mySlides')
-if (mySlides) {
-    showSlides();
-}
-
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    var sliedsContainer = document.querySelector(".slidesContainer")
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    var background = `${slideIndex + 1 + '.webp'}`
-    if (sliedsContainer.classList.contains('home')) {
-        sliedsContainer.style.backgroundImage = `url('/img/homeSlides/${background}')`
-    } else {
-        sliedsContainer.style.backgroundImage = `url('/img/slides/${background}')`
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 5000);
-}
