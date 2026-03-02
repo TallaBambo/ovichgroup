4fbf0833440a0d6d9a8167bd0ffdb5079f209c87
TallaBambo <109511549+TallaBambo@users.noreply.github.com>
2026-03-02 08:40:19 +0100

    Fix duplicate dotenv config in server.js
    Removed duplicate dotenv configuration call.

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' })
 const app = require('./app')
 
 process.on('uncaughtException', err => {
     process.exit(1)
 })
-
-dotenv.config({ path: './config.env' })
 
 const port = process.env.PORT || 3000
 const server = app.listen(port, () => {
